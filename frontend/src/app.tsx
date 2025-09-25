2be6c146c4577484f50a5b4525b69503c05ca5ec
ibnoukhalkanezakaria <ibnoukhalkanezakaria@gmail.com>
2025-09-25 22:08:39 +0100

    init

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,6 +3,7 @@ import { Router, Link } from "./library/Router/Router";
 import { FunctionComponent } from "./library/types/types";
 import Login from "./screens/Login/Login";
 import Home from "./screens/Home/Home";
+import SignUp from "./screens/SignUp/SignUp";
 
 const App: FunctionComponent = () => {
   return (
@@ -10,6 +11,7 @@ const App: FunctionComponent = () => {
       <main>
         <Router path="/" Component={Home} />
         <Router path="/login" Component={Login} />
+        <Router path="/signup" Component={SignUp} />
       </main>
     </div>
   );
